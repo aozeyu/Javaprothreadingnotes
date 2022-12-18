e3a3378db8d83a1d8587cf2f58198111e4be4cf3
aozeyu <3484766443@qq.com>
2022-12-18 18:50:28 +0800

    feat: add code

--- src/main/java/com/example/javaprothreadingnotes/lizi2/EnumOrder.java
+++ src/main/java/com/example/javaprothreadingnotes/lizi2/EnumOrder.java
@@ -4,6 +4,7 @@ public class EnumOrder {
   public static void main(String[] args) {
     for (Spiciness s : Spiciness.values()) {
       System.out.println(s + ",ordinal " + s.ordinal());
+      System.out.println("hello world");
     }
   }
 }
