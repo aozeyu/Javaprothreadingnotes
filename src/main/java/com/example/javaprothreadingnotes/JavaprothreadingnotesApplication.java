063056fb51f0e742356c758f1ee181576a02afd3
aozeyu <3484766443@qq.com>
2022-12-18 11:01:26 +0800

    Initial commit

---
+++ src/main/java/com/example/javaprothreadingnotes/JavaprothreadingnotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprothreadingnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprothreadingnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprothreadingnotesApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/javaprothreadingnotes/lizi/Bowl.java
@@ -0,0 +1,2 @@
+package com.example.javaprothreadingnotes.lizi;public class Bowl {
+}
