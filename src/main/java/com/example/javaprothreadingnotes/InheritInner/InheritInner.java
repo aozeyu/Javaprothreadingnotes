627304da1fdd9489351e354c1a9b78894225f735
aozeyu <3484766443@qq.com>
2022-12-27 18:42:44 +0800

    feat: add code

---
+++ src/main/java/com/example/javaprothreadingnotes/InheritInner/InheritInner.java
@@ -0,0 +1,13 @@
+package com.example.javaprothreadingnotes.InheritInner;
+
+//
+//class WithInner{
+//  class Inner{
+//
+//  }
+//}
+//public class InheritInner extends WithInner.Inner{
+//  InheritInner(WithInner wi) {
+//    wi.s
+//  }
+//}
