774957479db7a1a266828f20be459f73ce7413bf
aozeyu <3484766443@qq.com>
2023-01-03 18:42:32 +0800

    feat: add code

--- src/main/java/com/example/javaprothreadingnotes/zhouyu/Test2.java
+++ src/main/java/com/example/javaprothreadingnotes/zhouyu/Test2.java
@@ -1,29 +1,11 @@
 package com.example.javaprothreadingnotes.zhouyu;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Test2 {
-  String sonnet = "From fairest creatures we desire increase,\n" +
-      "That thereby beauty's rose might never die,\n" +
-      "But as the riper should by time decease\n" +
-      "His tender heir might bear his memory:\n" +
-      "But thou, contracted to thine own bright eyes,\n" +
-      "Feed'st thy light's flame with self-substantial fuel,\n" +
-      "Making a famine where abundance lies,\n" +
-      "Thyself thy foe, to thy sweet self too cruel.\n" +
-      "Thou that art now the world's fresh ornament,\n" +
-      "And only herald to the gaudy spring,\n" +
-      "Within thine own bud buriest thy content,\n" +
-      "And, tender churl, mak'st waste in niggardly.\n" +
-      "Pity the world, or else this glutton be,\n" +
-      "To eat the world's due, by the grave and thee.";
-  Pattern pattern = Pattern.compile("\\bfame\\b");
-  Matcher matcher = Pattern.matcher(sonnet);
-  while (matcher.find()) {
-    String group = matcher.group();
-    int start = matcher.start();
-    int end = matcher.end();
-    System.out.println(group + " " + start + " " + end);
+  public static void main(String[] args) throws InterruptedException {
+    long start = System.currentTimeMillis();
+
+    Thread.sleep(2000);
+
+    System.out.println("Sleep time in ms = " + (System.currentTimeMillis() - start));
   }
 }
