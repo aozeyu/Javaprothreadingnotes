e5ee16fbab0652914b813e97b03f10efe3dee2b4
aozeyu <3484766443@qq.com>
2023-01-11 16:12:16 +0800

    feat: add code

---
+++ src/main/java/com/example/javaprothreadingnotes/mapTest/Replace.java
@@ -0,0 +1,9 @@
+package com.example.javaprothreadingnotes.mapTest;
+
+public class Replace {
+    public static void main(String[] args) {
+        String myStr = "Hello";
+        System.out.println(myStr.replace('l', 'y'));
+    }
+
+}
