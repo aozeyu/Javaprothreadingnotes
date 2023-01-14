e436dd320ed0d65526c27f592a97842df282e8dc
aozeyu <3484766443@qq.com>
2023-01-14 15:32:59 +0800

    feat: Aop

--- src/main/java/com/example/javaprothreadingnotes/Aop/TxService.java
+++ src/main/java/com/example/javaprothreadingnotes/Aop/TxService.java
@@ -1,18 +1,26 @@
 package com.example.javaprothreadingnotes.Aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class TxService {
 
-    @Pointcut("execution(public void com.example.javaprothreadingnotes.Aop.EmployeeDao.saveEmployee")
+    @Pointcut("execution(public void com.example.javaprothreadingnotes.Aop.EmployeeDao.saveEmployee())")
     public void p1(){
 
     }
     @Before("p1()")
     public  void  beginTx() {
         System.out.println("Tx Started");
+    }
+
+    @After("p1()")
+    public void commitTx() {
+        System.out.println("Tx Commited");
     }
 }
