c770fdb3214b41e0a1d0a4a7f7f96d5866a13089
aozeyu <3484766443@qq.com>
2023-01-14 22:02:55 +0800

    feat: add code

--- src/main/java/com/example/javaprothreadingnotes/Aop/EmployeeDao.java
+++ src/main/java/com/example/javaprothreadingnotes/Aop/EmployeeDao.java
@@ -3,6 +3,7 @@ package com.example.javaprothreadingnotes.Aop;
 import org.springframework.stereotype.Component;
 
 @Component
+//wsdsad
 public class EmployeeDao {
     public void saveEmployee() {
         System.out.println("FROM SAVE EMPLOYEE");
