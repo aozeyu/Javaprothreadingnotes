1d9c961aff25368f7e1dd58a3eab67592152c627
aozeyu <3484766443@qq.com>
2023-01-16 11:21:42 +0800

    feat: add code

--- src/main/java/com/example/javaprothreadingnotes/zhouyu/Test2.java
+++ src/main/java/com/example/javaprothreadingnotes/zhouyu/Test2.java
@@ -1,10 +1,13 @@
 package com.example.javaprothreadingnotes.zhouyu;
 
 public class Test2 {
+
+  public static final int MILLIS = 365;
+
   public static void main(String[] args) throws InterruptedException {
     long start = System.currentTimeMillis();
 
-    Thread.sleep(2000);
+    Thread.sleep(MILLIS);
 
     System.out.println("Sleep time in ms = " + (System.currentTimeMillis() - start));
   }
