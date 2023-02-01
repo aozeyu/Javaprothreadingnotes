04006f3055e06fc74589fe22a4cc7487288d3412
aozeyu <3484766443@qq.com>
2023-02-01 20:50:43 +0800

    feat: add code

---
+++ src/main/java/com/example/javaprothreadingnotes/Jedis/Diasdn.java
@@ -0,0 +1,19 @@
+package com.example.javaprothreadingnotes.Jedis;
+
+public class Diasdn {
+    public static void main(String[] args) {
+        int x =(int) (10 * 3.5 + 6 * 1.5);
+        System.out.println(x);
+        char c1 = 100; // ok
+        int m = 100; //ok
+        char c3 = (char) m; // int 转char
+        System.out.println(19.0/9.0);
+
+        for (int i=0; i < 5; i++){
+            for (int j=0; j<i; j++){
+                System.out.print('*');
+            }
+            System.out.print('\n');
+        }
+    }
+}
