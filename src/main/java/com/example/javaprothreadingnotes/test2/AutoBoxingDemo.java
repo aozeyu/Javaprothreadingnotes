13ee6a508d26d7b73ec4e0b66ad81d485a8145bc
aozeyu <3484766443@qq.com>
2023-02-04 21:52:50 +0800

    feat: add code

---
+++ src/main/java/com/example/javaprothreadingnotes/test2/AutoBoxingDemo.java
@@ -0,0 +1,13 @@
+package com.example.javaprothreadingnotes.test2;
+
+public class AutoBoxingDemo {
+  public static void main(String[] args) {
+    int i = 10;
+    Integer iObj = Integer.valueOf(i);
+    //装箱
+    System.out.println(iObj);
+    int i1 = iObj;
+    //开箱
+    System.out.println(i1);
+  }
+}
