b9d00fc6af46c971ca1205e9bbaf5618929c46f0
aozeyu <3484766443@qq.com>
2023-02-08 23:56:21 +0800

    feat: add code on feature2.0

---
+++ src/main/java/com/example/javaprothreadingnotes/test2/fetureBranch.java
@@ -0,0 +1,7 @@
+package com.example.javaprothreadingnotes.test2;
+
+public class fetureBranch {
+  public static void main(String[] args) {
+    System.out.println("I am freeking out right now!!");
+  }
+}
