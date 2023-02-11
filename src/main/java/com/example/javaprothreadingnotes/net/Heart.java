e480a3bcf02f95badc0c453940f1f0800580b042
aozeyu <3484766443@qq.com>
2023-02-11 19:15:12 +0800

    feat: add code

---
+++ src/main/java/com/example/javaprothreadingnotes/net/Heart.java
@@ -0,0 +1,11 @@
+package com.example.javaprothreadingnotes.net;
+
+public class Heart {
+    public static void main(String[] args) {
+        System.out.println("    ❤");
+        System.out.println(" ❤❤❤");
+        System.out.println("❤❤❤❤❤");
+        System.out.println(" ❤❤❤");
+        System.out.println("    ❤");
+    }
+}
