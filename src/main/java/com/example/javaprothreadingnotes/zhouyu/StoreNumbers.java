91795619b192e91e57166e858f1c36945d119b7e
aozeyu <3484766443@qq.com>
2023-02-18 13:19:09 +0800

    feat: add code

---
+++ src/main/java/com/example/javaprothreadingnotes/zhouyu/StoreNumbers.java
@@ -0,0 +1,21 @@
+package com.example.javaprothreadingnotes.zhouyu;
+
+public class StoreNumbers {
+  private boolean[] numbers = new boolean[100];
+
+  public void store(int num) {
+    numbers[num] = true;
+  }
+
+  public boolean isStored(int num) {
+    return numbers[num];
+  }
+
+  public static void main(String[] args) {
+    StoreNumbers sn = new StoreNumbers();
+    //初始是false
+    System.out.println(sn.isStored(2));
+    sn.store(2);
+    System.out.println(sn.isStored(2));
+  }
+}
