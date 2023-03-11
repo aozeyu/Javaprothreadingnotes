1ba480815c25bcf2b007de63dd8bd52762ff6e73
aozeyu <3484766443@qq.com>
2023-03-11 11:33:45 +0800

    feat: dugger

---
+++ src/main/java/com/example/javaprothreadingnotes/debugger/Debug02.java
@@ -0,0 +1,12 @@
+package com.example.javaprothreadingnotes.debugger;
+
+public class Debug02 {
+  public static void main(String[] args) {
+    String flag = "1";
+    if (flag.equals("520")) {
+      System.out.println("我还能");
+    }else {
+      System.out.println("213");
+    }
+  }
+}
