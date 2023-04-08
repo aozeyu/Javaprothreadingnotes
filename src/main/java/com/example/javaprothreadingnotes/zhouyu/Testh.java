df7f8f44f5862ac8b7bf4b4c993c0d9af508168d
aozeyu <3484766443@qq.com>
2023-04-08 12:54:24 +0800

    ,6

---
+++ src/main/java/com/example/javaprothreadingnotes/zhouyu/Testh.java
@@ -0,0 +1,15 @@
+package com.example.javaprothreadingnotes.zhouyu;
+
+public class Testh {
+  public static void main(String[] args) {
+      int result = sum(5, 10);
+    System.out.println(result);
+  }
+  public static int sum(int start, int end) {
+    if (end > start) {
+      return end + sum(start, end -1);
+    }else  {
+      return  end;
+    }
+  }
+}
