90a0e0841a23fd8f677d877d8df699e0974dc3a7
aozeyu <3484766443@qq.com>
2023-04-30 21:33:29 +0800

    add

---
+++ src/main/java/com/example/javaprothreadingnotes/base/Singleton.java
@@ -0,0 +1,14 @@
+package com.example.javaprothreadingnotes.base;
+
+public class Singleton {
+  private static Singleton instance;
+  private Singleton() {
+
+  }
+  public static Singleton getInstance() {
+    if (instance == null) {
+      instance = new Singleton();
+    }
+    return instance;
+  }
+}
