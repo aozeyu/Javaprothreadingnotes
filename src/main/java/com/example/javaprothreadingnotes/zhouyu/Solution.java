762bc9d6adcacd10798933d5906834d6caf6efc7
aozeyu <3484766443@qq.com>
2023-05-27 22:16:08 +0800

    commit

---
+++ src/main/java/com/example/javaprothreadingnotes/zhouyu/Solution.java
@@ -0,0 +1,15 @@
+package com.example.javaprothreadingnotes.zhouyu;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+        int c = x;
+        //打印x的值
+        System.out.println("c= " + c);
+
+    }
+}
